62bcf46d3f0fc8930db66f5b167dbd2475d0959c
nhan.tran@bowaresolutions.com <Nhan Boware>
2025-04-23 17:26:45 +0700

    Outline: Http Interceptor

---
+++ ECommerce.Infrastructure/Managers/Interceptors/HttpInterceptorManager.cs
@@ -0,0 +1,48 @@
+﻿using ECommerce.Infrastructure.Managers.Identity.Authentication;
+using Microsoft.AspNetCore.Components;
+using Toolbelt.Blazor;
+
+namespace ECommerce.Infrastructure.Managers.Interceptors;
+
+public class HttpInterceptorManager : IHttpInterceptorManager
+{
+    private readonly HttpClientInterceptor _interceptor;
+    private readonly IAuthenticationManager _authenticationManager;
+    private readonly NavigationManager _navigationManager;
+
+    public HttpInterceptorManager(
+        HttpClientInterceptor interceptor,
+        IAuthenticationManager authenticationManager,
+        NavigationManager navigationManager)
+    {
+        _interceptor = interceptor;
+        _authenticationManager = authenticationManager;
+        _navigationManager = navigationManager;
+    }
+
+    public void RegisterEvent() => _interceptor.BeforeSendAsync += InterceptBeforeHttpAsync;
+
+    public async Task InterceptBeforeHttpAsync(object sender, HttpClientInterceptorEventArgs e)
+    {
+        var absPath = e.Request.RequestUri!.AbsolutePath;
+        if (!absPath.Contains("token") && !absPath.Contains("accounts"))
+        {
+            try
+            {
+                //var token = await _authenticationManager.RefreshTokenAsync();
+                //if (!string.IsNullOrEmpty(token))
+                //{
+                //    e.Request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                //}
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                await _authenticationManager.Logout();
+                _navigationManager.NavigateTo("/");
+            }
+        }
+    }
+
+    public void DisposeEvent() => _interceptor.BeforeSendAsync -= InterceptBeforeHttpAsync;
+}
---
+++ ECommerce.Infrastructure/Managers/Interceptors/IHttpInterceptorManager.cs
@@ -0,0 +1,12 @@
+﻿using Toolbelt.Blazor;
+
+namespace ECommerce.Infrastructure.Managers.Interceptors;
+
+public interface IHttpInterceptorManager : IManager
+{
+    void RegisterEvent();
+
+    Task InterceptBeforeHttpAsync(object sender, HttpClientInterceptorEventArgs e);
+
+    void DisposeEvent();
+}
