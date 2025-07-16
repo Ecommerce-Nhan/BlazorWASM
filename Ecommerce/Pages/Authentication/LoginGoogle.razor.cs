e43c5d0ce6dc1458eb798075d76a47be5d11ec8f
nhan.tran@bowaresolutions.com <Nhan Boware>
2025-07-16 14:45:15 +0700

    Outline google

---
+++ Ecommerce/Pages/Authentication/LoginGoogle.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.NetworkInformation;
+using System.Web;
+
+namespace Ecommerce.Pages.Authentication;
+
+public partial class LoginGoogle : ComponentBase
+{
+    private string? _status;
+    protected override async Task OnInitializedAsync()
+    {
+        var uri = _navigationManager.ToAbsoluteUri(_navigationManager.Uri);
+        var code = HttpUtility.ParseQueryString(uri.Query).Get("code");
+
+        if (string.IsNullOrWhiteSpace(code))
+        {
+            _status = "Google login failed: missing code.";
+            return;
+        }
+    }
+}
